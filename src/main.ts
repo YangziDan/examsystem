6773bed47e533d31e4741b54e96c6bf7387078cd
YangziDan <2014344591@qq.com>
2023-12-02 12:08:23 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
