2f4d4b127411c4760ac661b4915536fb93ec5855
YangziDan <2014344591@qq.com>
2023-12-09 10:12:10 +0800

    将loginview前后端相连

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,50 +1,49 @@
 <script setup>
 import {ref} from "vue";
-
+import axios from "axios";
+import cookies from "vue-cookies"
+import router from "@/router";
 let role = ref(2);
 let labelPosition = ref('left');
 let formLabelAlign = ref({
-  username: '2023',
+  username: '1',
   password: '123456'
 })
 
 function login() {
   console.log("登录操作执行-------");
-  this.$axios({
-    url: `/api/login`,
+  axios({
+    url: `http://localhost:8999/login`,
     method: 'post',
     data: {
-      ...this.formLabelAlign
+      username:formLabelAlign.value.username,
+      password:formLabelAlign.value.password
     }
   }).then(res => {
     let resData = res.data.data
     if (resData != null) {
       switch (resData.role) {
         case "0":  //管理员
-          this.$cookies.set("cname", resData.adminName)
-          this.$cookies.set("cid", resData.adminId)
-          this.$cookies.set("role", 0)
-          this.$router.push({path: '/index'}) //跳转到首页
+          cookies.set("cname", resData.adminName)
+          cookies.set("cid", resData.adminId)
+          cookies.set("role", 0)
+          router.push({path: '/index'}) //跳转到首页
           break
         case "1": //教师
-          this.$cookies.set("cname", resData.teacherName)
-          this.$cookies.set("cid", resData.teacherId)
-          this.$cookies.set("role", 1)
-          this.$router.push({path: '/index'}) //跳转到教师用户
+          cookies.set("cname", resData.teacherName)
+          cookies.set("cid", resData.teacherId)
+          cookies.set("role", 1)
+          router.push({path: '/index'}) //跳转到教师用户
           break
         case "2": //学生
-          this.$cookies.set("cname", resData.studentName)
-          this.$cookies.set("cid", resData.studentId)
-          this.$router.push({path: '/student'})
+          cookies.set("cname", resData.studentName)
+          cookies.set("cid", resData.studentId)
+          router.push({path: '/student'})
           break
       }
     }
     if (resData == null) { //错误提示
-      this.$message({
-        showClose: true,
-        type: 'error',
-        message: '用户名或者密码错误'
-      })
+      console.log("resData is null")
     }
   })
 }
