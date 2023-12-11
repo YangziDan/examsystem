1b0238b96a7daa1d00c5ee95cc48c1e07620612f
浪漫的飞猪 <1447374773@qq.com>
2023-12-11 10:25:24 +0800

    完善个人中心

--- src/components/Zjh/InfoList.vue
+++ src/components/Zjh/InfoList.vue
@@ -1,11 +1,13 @@
 
 <script setup lang="ts">
 import {useUserinfoStore} from "@/stores/UserInfo";
-
+import cookies from "vue-cookies";
+import {ref} from "vue";
 const store=useUserinfoStore()
-
+const userId = ref(cookies.get('cid'))
 </script>
 <template>
+
   <el-descriptions
       title="用户基本信息"
       :column="4"
@@ -13,7 +15,7 @@ const store=useUserinfoStore()
       direction="vertical"
       :style="blockMargin"
   >
-    <el-descriptions-item label=ID>1</el-descriptions-item>
+    <el-descriptions-item label=ID>{{userId}}</el-descriptions-item>
     <el-descriptions-item label="用户名">{{store.UserName}}</el-descriptions-item>
     <el-descriptions-item label="电话">{{store.UserPhone}}</el-descriptions-item>
     <el-descriptions-item label="年级" :span="2">{{store.UserGrade[0]}}</el-descriptions-item>
