45191f599dcbd7390fcb31d476bd8ad0cc54fe67
YangziDan <2014344591@qq.com>
2023-12-15 15:53:18 +0800

    在package里面引入了echart

--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,51 +1,33 @@
 <template>
-  <div class="wrapper">
-    <el-container>
-      <el-aside width="35vw">
-        <manager-left></manager-left>
-      </el-aside>
-      <el-container>
-        <el-header>
-          <p class="title">
-            管理员后台界面
-          </p>
-        </el-header>
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-      </el-container>
-    </el-container>
-  </div>
+
 </template>
 <script setup>
-
-import ManagerLeft from "@/components/ljk/managerLeft.vue";
+import * as echarts from 'echarts';
+// 基于准备好的dom，初始化echarts实例
+let myChart = echarts.init(document.getElementById('main'));
+// 绘制图表
+myChart.setOption({
+  title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
 </script>
 
 
 <style scoped>
-.wrapper {
-  width: 94vw;
-  height: 94vh;
-  display: flex;
-  justify-content: center;
-  /*align-items: center;*/
-}
-.title {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  color: #2f6c9f;
-  height: 7vw;
-}
 
-
-.com {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
 </style>
 
